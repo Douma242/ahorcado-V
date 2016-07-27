e9e56e7f5a980235fcbd442d4a6f82958e8221e7
Douma242 <danieldouma@hotmail.es>
2016-07-27 09:43:26 -0600

    :D

--- ahorcado.py
+++ ahorcado.py
@@ -1,16 +1,22 @@
 import funciones
 print("Bienvenido")
 palabras=[]
-i=[1,2,3,4,5,6,7,8]
+guiones=[]
+gana=False
+errores=0
 p=funciones.palabritas(palabras)
-lenp=len(p)
-oculto="_ "*lenp
+for letra in p:
+	guiones.append("_")
+while errores<=7:
+	while "_" in guiones:
+		oculto=" ".join(guiones)
+		print (p)
+		print(oculto)
+		res=input("Coloque letra: ")
+		for i in range (0, len(p)):
+			if p[i]==res:
+				guiones[i]=res
+		if res not in guiones:
+			errores=errores+1
 print (p)
-print(oculto)
-pa=list(p)
-res=input("Coloque letra: ")
-if res in pa:
-	gana=True
-else:
-	print("Pato")
-
+print ("Bai Bai")
